11cb8ce2a1f89f2679b8ccfe13333d1dc44c1e24
hebasta <stallkamp@ids-mannheim.de>
2020-03-07 13:44:17 +0100

    Add configuration file for host and port
    Change-Id: I54e3c1af23c440c21a725f1caaad6d65a4fdadaa

--- plugin/src/test/java/de/ids_mannheim/korap/plkexport/IdsExportServiceTest.java
+++ plugin/src/test/java/de/ids_mannheim/korap/plkexport/IdsExportServiceTest.java
@@ -1,4 +1,4 @@
-package de.ids_mannheim.korap.pklexport;
+package de.ids_mannheim.korap.plkexport;
 
 import static org.junit.Assert.assertEquals;
 
@@ -6,7 +6,9 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response.Status;
+import java.net.URI;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
@@ -25,12 +27,20 @@ public class IdsExportServiceTest extends JerseyTest {
 
 	   @Test
 	   public void testExportWs() {
-		   Response response = target("/ids-export/exportHtml").request().get();
+           WebTarget t = target("/ids-export/exportHtml");
+		   Response response = t.request().get();
 		   assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
 		   assertEquals("Http Content-Type should be: ", MediaType.TEXT_HTML, response.getHeaderString(HttpHeaders.CONTENT_TYPE));
 		   String content = response.readEntity(String.class);
 		   assertEquals("Content of response is: ", "Export Web Service under construction", content);
-	   } 
+
+           URI uri = t.getUri();
+           assertEquals("Host is: ", "localhost", uri.getHost());
+
+           // Expected: 7777 or 7070
+           assertEquals("Port is: ", 7777, uri.getPort());
+           assertEquals("URI is: ", "http://localhost:7777/ids-export/exportHtml", uri.toString());
+       }
 
 }
 
