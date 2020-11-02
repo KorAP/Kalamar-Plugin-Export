37926caf3a174bfe0c296eeee99d4e5d291dfab3
hebasta <stallkamp@ids-mannheim.de>
2020-11-02 10:00:26 +0100

    playAround
    Change-Id: If61897f421cd1df9a5127af04db0e2c350fd8675

--- plugin/src/main/java/de/ids_mannheim/korap/plkexport/ExWSConf.java
+++ plugin/src/main/java/de/ids_mannheim/korap/plkexport/ExWSConf.java
@@ -16,9 +16,18 @@ public class ExWSConf {
      * See also: https://www.ids-mannheim.de/cosmas2/script-app/hilfe/sitzung.html
      */
     public static final int MAX_EXP_LIMIT = 10000;
+    /* 
+     * TODO:
+     * Analog zur Variable aus Search.pm 
+     * Kommentar eventuell JSON um aus Perl und Java einzulesen 
+     */
+    //Eigentlich 25 zu Testzwecken kleiner
+ //   public static final int PAGE_SIZE = 25;
+    public static final int PAGE_SIZE = 5;
     
     //Version of Export Plugin
     public static final int VERSION_MAJOR = 0;
     public static final int VERSION_MINOR = 1;
     public static final int VERSION_PATCHLEVEL= 0;
+    
 }
