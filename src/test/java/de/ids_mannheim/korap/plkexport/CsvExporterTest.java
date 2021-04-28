7a432e7bda8538027de17c8130cccc75e3666777
Akron <nils@diewald-online.de>
2021-04-28 17:57:41 +0200

    Fix nullpointer exception in CSV exporter
    Change-Id: Ie49251f79c2fe5b1b702375b8776180bd2112c63

--- src/test/java/de/ids_mannheim/korap/plkexport/CsvExporterTest.java
+++ src/test/java/de/ids_mannheim/korap/plkexport/CsvExporterTest.java
@@ -56,6 +56,28 @@ public class CsvExporterTest {
         assertEquals(lines[1],",Simple,match1,Snippet,,,RTF/G59/34284,Goethe,20051103,Title1");
         assertEquals(lines[2],"...,\"Simpler, \"\"faster\"\"\",\"\"\"match2\"\"\",Snippet,...,,RTF/G59/34285,Schiller,20051104,\"Title2, the\"");
         assertEquals(lines.length,3);
+
+        csv = new CsvExporter();
+        csv.init("{\"matches\":["+
+                 "{\"title\":\"Title1\","+
+                 "\"pubDate\":\"20051103\","+
+                 "\"textSigle\":\"RTF/G59/34284\","+
+                 "\"snippet\":\"Simple <mark>match1</mark> Snippet\"}"+
+                 ","+
+                 "{\"author\":\"Schiller\","+
+                 "\"title\":\"Title2, the\","+
+                 "\"pubDate\":\"20051104\","+
+                 "\"textSigle\":\"RTF/G59/34285\","+
+                 "\"snippet\":\"<span class=\\\"context-left\\\"><span class=\\\"more\\\"></span>"+
+                 "Simpler, \\\"faster\\\" </span><span class=\\\"match\\\"><mark>&quot;match2&quot;</mark></span>"+
+                 "<span class=\\\"context-right\\\"> Snippet"+
+                 "<span class=\\\"more\\\"></span></span>\"}"+
+                 "]}");
+        csv.finish();
+        resp = csv.serve().build();
+        x = (String) resp.getEntity();
+        resp.close();
+
     };
 
     @Test
