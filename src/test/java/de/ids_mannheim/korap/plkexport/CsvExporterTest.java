8875c73e69e4455c19af78f728bb21bf3ca2266f
Akron <nils@diewald-online.de>
2021-05-27 10:55:13 +0200

    Sanitize CSV export for security reasons
    This follows the recommendations in
    https://wiki.mozilla.org/images/6/6f/Phpmyadmin-report.pdf to sanitize
    the export, preventing the execution of arbitrary code in spreadsheet
    editors such as Excel.
    
    Change-Id: Icb91c425f6fec0bc3d7f83ad3bdcc7b11b483983

--- src/test/java/de/ids_mannheim/korap/plkexport/CsvExporterTest.java
+++ src/test/java/de/ids_mannheim/korap/plkexport/CsvExporterTest.java
@@ -54,7 +54,7 @@ public class CsvExporterTest {
         String[] lines = x.split("\n");
         assertEquals(lines[0],"HasMoreLeft,leftContext,Match,rightContext,HasMoreRight,isCutted,textSigle,author,pubDate,title");
         assertEquals(lines[1],",Simple,match1,Snippet,,,RTF/G59/34284,Goethe,20051103,Title1");
-        assertEquals(lines[2],"...,\"Simpler, \"\"faster\"\"\",\"\"\"match2\"\"\",Snippet,...,,RTF/G59/34285,Schiller,20051104,\"Title2, the\"");
+        assertEquals(lines[2],"...,\"Simpler, \"\"faster\"\"\",\" \"\"match2\"\"\",Snippet,...,,RTF/G59/34285,Schiller,20051104,\"Title2, the\"");
         assertEquals(lines.length,3);
 
         csv = new CsvExporter();
@@ -80,6 +80,38 @@ public class CsvExporterTest {
 
     };
 
+    @Test
+    public void testCsvSanitize () throws IOException {
+        CsvExporter csv = new CsvExporter();
+        csv.init("{\"meta\":\"ja\",\"collection\":\"hm\",\"query\":\"cool\"," +
+                 "\"matches\":["+
+                 "{\"author\":\"Goethe\","+
+                 "\"title\":\"@Title1\","+
+                 "\"pubDate\":\"20051103\","+
+                 "\"textSigle\":\"RTF/G59/34284\","+
+                 "\"snippet\":\"=Simple <mark>-match1</mark> +Snippet\"}"+
+                 ","+
+                 "{\"author\":\"+Schiller\","+
+                 "\"title\":\"-Title2,\\tthe\","+
+                 "\"pubDate\":\"20051104\","+
+                 "\"textSigle\":\"RTF/G59/34285\","+
+                 "\"snippet\":\"<span class=\\\"context-left\\\"><span class=\\\"more\\\"></span>"+
+                 "Simpler, \\\"faster\\\" </span><span class=\\\"match\\\"><mark>&quot;match2&quot;</mark></span>"+
+                 "<span class=\\\"context-right\\\"> Snippet"+
+                 "<span class=\\\"more\\\"></span></span>\"}"+
+                 "]}");
+        csv.finish();
+
+        Response resp = csv.serve().build();
+        String x = (String) resp.getEntity();
+        resp.close();
+        String[] lines = x.split("\n");
+        assertEquals(lines[0],"HasMoreLeft,leftContext,Match,rightContext,HasMoreRight,isCutted,textSigle,author,pubDate,title");
+        assertEquals(lines[1],",\" =Simple\",\" -match1\",\" +Snippet\",,,RTF/G59/34284,Goethe,20051103,\" @Title1\"");
+        assertEquals(lines[2],"...,\"Simpler, \"\"faster\"\"\",\" \"\"match2\"\"\",Snippet,...,,RTF/G59/34285,\" +Schiller\",20051104,\" -Title2, the\"");
+        assertEquals(lines.length,3);
+    };
+  
     @Test
     public void testAttributes () throws IOException {
         CsvExporter csv = new CsvExporter();
